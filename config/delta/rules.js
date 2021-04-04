2a80261e4c9f67d8ac04ed3931d53fcfaafe5468
Erika Pauwels <erika.pauwels@gmail.com>
2021-04-04 21:41:45 +0200

    Add delta-notifier

---
+++ config/delta/rules.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    match: {
+      // match everything
+    },
+    callback: {
+      url: 'http://resource/.mu/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 250,
+      ignoreFromSelf: true
+    }
+  }
+];
