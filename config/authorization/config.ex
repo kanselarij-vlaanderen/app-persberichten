54384d18817224312769e2d712ffcafdaa4b33f0
Erika Pauwels <erika.pauwels@gmail.com>
2021-08-18 16:40:08 +0200

    Add read permission to the session graph for all users

--- config/authorization/config.ex
+++ config/authorization/config.ex
@@ -2,6 +2,7 @@ alias Acl.Accessibility.Always, as: AlwaysAccessible
 alias Acl.Accessibility.ByQuery, as: AccessByQuery
 alias Acl.GraphSpec.Constraint.Resource, as: ResourceConstraint
 alias Acl.GraphSpec.Constraint.Resource.NoPredicates, as: NoPredicates
+alias Acl.GraphSpec.Constraint.ResourceFormat, as: ResourceFormatConstraint
 alias Acl.GraphSpec, as: GraphSpec
 alias Acl.GroupSpec, as: GroupSpec
 alias Acl.GroupSpec.GraphCleanup, as: GraphCleanup
@@ -104,6 +105,12 @@ defmodule Acl.UserGroups.Config do
                 codelists_resource_types() ++
                 authentication_resource_types()
               ]
+            }
+          },
+          %GraphSpec{
+            graph: "http://mu.semte.ch/graphs/sessions",
+            constraint: %ResourceFormatConstraint{
+              resource_prefix: "http://mu.semte.ch/sessions/"
             }
           }
         ]
