4716f8de9ebae6d78d3a0a41f5220fd55d5262ad
Michael <30836085+MiVvlt@users.noreply.github.com>
2021-08-24 20:11:20 +0200

    delta rule configuration for email-generator service

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -29,7 +29,26 @@ export default [
             ignoreFromSelf: true,
         },
     },
+    {
+        match: {
+            predicate: {
+                type: 'uri',
+                value: 'http://www.w3.org/ns/adms#status',
+            },
+            object: {
+                type: 'uri',
+                value: 'http://themis.vlaanderen.be/id/concept/publication-task-status/not-started',
+            },
+        },
+        callback: {
+            url: 'http:/press-release-email-generator/delta', method: 'POST',
+        },
+        options: {
+            resourceFormat: 'v0.0.1',
+            gracePeriod: 250,
+            ignoreFromSelf: true,
+        },
+    },
 ];
-
 
 
